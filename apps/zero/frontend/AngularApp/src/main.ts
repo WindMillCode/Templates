e2bf021204fb44e424109aa84c0557de86641f5c
windmillcode0 <shieldmousetower734@gmail.com>
2023-04-20 18:20:00 -0400

    [UPDATE] additional work

--- apps/zero/frontend/AngularApp/src/main.ts
+++ apps/zero/frontend/AngularApp/src/main.ts
@@ -8,22 +8,23 @@ import { AppModule } from './app/app.module';
 import { ENV, environment } from './environments/environment';
 
 
-Sentry.init({
-  environment:ENV.type,
-  dsn: "https://7207fc735fe940ae94e84956a91ac2f9@o487417.ingest.sentry.io/4504860386394112",
-  integrations: [
-    new BrowserTracing({
-      tracePropagationTargets: ["localhost", "https://yourserver.io/api"],
-      routingInstrumentation: Sentry.routingInstrumentation,
-    }),
-  ],
-  // beforeSend
+// Sentry.init({
+//   environment:ENV.type,
+//   dsn: "https://[YOUR SENTRY DSN URL HERE]",
+//   integrations: [
+//     new BrowserTracing({
+//       tracePropagationTargets: ["localhost", "https://yourserver.io/api"],
+//       routingInstrumentation: Sentry.routingInstrumentation,
+//     }),
+//   ],
+//   // beforeSend
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+//   // Set tracesSampleRate to 1.0 to capture 100%
+//   // of transactions for performance monitoring.
+//   // We recommend adjusting this value in production
+//   tracesSampleRate: 1.0,
+// });
+
 
 
 if (environment.production) {
